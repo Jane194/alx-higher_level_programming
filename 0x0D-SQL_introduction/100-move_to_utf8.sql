bb2c5f0b84c255230840247a95e58bd16c4ead27
Jane194 <janetaklah@gmail.com>
2024-02-13 05:51:52 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- source https://dba.stackexchange.com/questions/8239/how-to-easily-convert-utf8-tables-to-utf8mb4-in-mysql-5-5
 USE hbtn_0c_0
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_c;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
